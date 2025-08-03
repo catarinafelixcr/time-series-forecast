80cfc0462c01087bb5ee9b26fdf5d52bc4e6ad11
Catarina <catafcruz02@gmail.com>
2025-08-03 22:17:58 +0100

    1st commit of time series project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='i will do a project with a lot of search to apply time series techniques for 1st time correctly',
+    author='catarina',
+    license='',
+)
